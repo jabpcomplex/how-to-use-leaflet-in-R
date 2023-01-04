fd01575a9e17701aa341fa87a88ece796fa36b18
jabpcomplex <86539158+jabpcomplex@users.noreply.github.com>
2023-01-04 10:28:05 -0600

    clase1 leaflet

---
+++ clase1_base_principal.R
@@ -0,0 +1,38 @@
+
+#Ubicación de líneas y estaciones del Sistema de Transporte Colectivo Metro
+#Geolocalización de líneas y estaciones del Sistema de Transporte Colectivo Metro
+#creado en  R version 4.2.2 (1-01-2022)
+
+#agregamos ruta de trabajo
+setwd("/home/jabp/Programacion/R-scripts/TallerR/MapasR/leaflet/")
+
+
+#instale la libreria leaflet
+install.packages("leaflet")
+
+#Esqueleto principal para crear mapas con leaflet
+#1.- Cree un widget de mapa llamando a leaflet().
+#2.- Agregue capas (es decir, características) al mapa mediante el uso de funciones de capa (por ejemplo, addTiles, addMarkers, addPolygons) para modificar el widget del mapa.
+#3.- Repita el paso 2 según lo desee.
+#4.- Imprima el widget de mapa para mostrarlo.
+
+
+library(leaflet)
+
+mapa <- leaflet() %>%
+        addTiles() %>%  # Agregar capas de mapa predeterminados de OpenStreetMap
+        addMarkers(lng=-99.1441192, lat=19.4357619, popup="Alameda central Ciudad de México")%>%
+        setView(lng=-99.1441192, lat=19.4357619, zoom = 15)
+mapa  # imprime el mapa
+
+
+
+mapa2 <- leaflet() %>%
+  addTiles() %>%  # Agregar capas de mapa predeterminados de OpenStreetMap
+  addMarkers(lng=-99.1441192, lat=19.4357619, popup="Alameda central Ciudad de México")%>%
+  setView(lng=-99.1441192, lat=19.4357619, zoom = 15)%>%
+  #Podemos utilizar servicios de teselas de terceros. Ya hemos visto anteriormente el ejemplo de OpenStreetMap,
+  #que es el que R utiliza por defecto, pero también tenemos otros disponibles.
+  addProviderTiles(providers$Stamen.Toner)
+
+mapa2  # imprime el mapa
