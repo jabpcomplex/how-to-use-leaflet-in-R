d31130a2c2f26b5c6f4c09b136efbf19bf232ffa
Julio Alberto <86539158+jabpcomplex@users.noreply.github.com>
2025-02-28 11:53:25 -0600

    Update clase3_camaras_xotepingo.R
    update mapa xotepingo

--- clase3_camaras_xotepingo.R
+++ clase3_camaras_xotepingo.R
@@ -1,41 +1,19 @@
-
-#LIBRERIAS--------
-rm(list = ls())
-library(readxl)
-library(cleangeo)
-library(deldir)
-library(dplyr)
-library(foreign)
-library(ggmap) 
-library(ggplot2) 
-library(htmltools)
-library(htmlwidgets)
+library(sf)
 library(leaflet)
 library(leaflet.extras)
-library(mapproj)
-library(maptools)
-library(mapview)
-library(raster)
-library(readr)
-library(rgdal)
-library(spatstat) 
-library(sp) 
-library(tidyr)
-library(webshot)
-library(graphics)
-library(leafem)
-
-setwd("C:/Users/jbautistap/Documents/ArchivosC5/R/productos html/xotepingo/")
+library(htmltools)
+library(htmlwidgets)
+library(tidyverse)
+library(dplyr)
+library(readxl)
 
 ########################################################################################
 #cargamos los poligonos para colonias y sectores
-colRel <- spTransform(shapefile("COL_EL RELOJ.shp"), "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0")
-secXote <- spTransform(shapefile("SECTOR_XOTEPINGO.shp"), "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0")
-#shpf <- readOGR("SECTOR_XOTEPINGO.shp",verbose=FALSE,use_iconv = TRUE, encoding = "UTF-8") # encoding="latin1", use_iconv=FALSE)
+colRel <- spTransform(shapefile("RUTA/ARCHIVO/COL_EL RELOJ.shp"), "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0")
+secXote <- spTransform(shapefile("RUTA/ARCHIVO/SECTOR_XOTEPINGO.shp"), "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0")
 
 # base con las camaras STV (Sistemas Tecnologicos de Videovigilancia)
-camaras <- read_excel("SECTOR XOTEPINGO_COL_EL RELOJ.xlsx")
-
+camaras <- read_excel("RUTA/ARCHIVO/SECTOR XOTEPINGO_COL_EL RELOJ.xlsx")
 
 ########################################################
 #     Exploramos los datos
@@ -51,8 +29,6 @@ cam9<-camaras[camaras$TIPO_POSTE=="9m",]
 cam20<-camaras[camaras$TIPO_POSTE=="20m",]
 #Filtramos los TOTEMS
 totem<-camaras[camaras$TIPO_POSTE=="TOTEM",]
-totemF2<-camaras[camaras$TIPO_POSTE=="TOTEM_FASE2",]
-totem<-rbind(totem,totemF2)
 
 
 ################################################################################################################
@@ -79,46 +55,9 @@ title <- tags$div(
   tag.map.title, HTML(paste("Camaras en Coyoacán (Xotepingo)"))
 )
 
-######################################################################################
-#imagen
-imagen1<-iconList(city=makeIcon(iconUrl = "https://i.ibb.co/7131jWL/logo-reducido.png"))
-
-######################################################################################
 #Etiquetas personalizadas
-map <- c("#1F77B4","#FF7F0E","#2CA02C")#,"#D62728","#9467BD","#8C564B","#358000","#17BECF")
-colors <- c("#7F7F7F","#17BECF","red","blue","#358000")
-# Calculamos la dimensión para las etiquetas
-labels <- c("SECTORES","COLONIAS",
-            paste("9m","(",nrow(cam9),")"),
-            paste("20m","(",nrow(cam20),")"),
-            paste("TÓTEMS","(",nrow(totem),")"))
-
-sizes <- c(10,10,10,10,10)
-shapes <- c("square","square","circle","circle","circle")
-borders <- c("#7F7F7F","#17BECF","red","blue","#358000")
-
-####
-#Funcion para crear etiquetas
-addLegendCustom <- function(map, colors, labels, sizes, shapes, borders, opacity = 0.5){
-  
-  make_shapes <- function(colors, sizes, borders, shapes) {
-    shapes <- gsub("circle", "50%", shapes)
-    shapes <- gsub("square", "0%", shapes)
-    paste0(colors, "; width:", sizes, "px; height:", sizes, "px; border:3px solid ", borders, "; border-radius:", shapes)
-  }
-  make_labels <- function(sizes, labels) {
-    paste0("<div style='display: inline-block;height: ", 
-           sizes, "px;margin-top: 4px;line-height: ", 
-           sizes, "px;'>", labels, "</div>")
-  }
-  
-  legend_colors <- make_shapes(colors, sizes, borders, shapes)
-  legend_labels <- make_labels(sizes, labels)
-  
-  return(addLegend(map, colors = legend_colors, labels = legend_labels, opacity = opacity,position = "bottomleft"))
-}
-###
-
+etiquetas_camara <- c("9m", "20m","TOTEM" ) 
+colores_camara <- c("blue","green","red") # Define los colores para cada categoría
 
 
 
@@ -127,16 +66,13 @@ addLegendCustom <- function(map, colors, labels, sizes, shapes, borders, opacity
 ####                 #GENERAMOS MAPA CON LEAFLET                                                           ####
 ################################################################################################################
 
-leaflet()  %>%addTiles() %>%addProviderTiles(providers$Esri.WorldGrayCanvas) %>% 
-  # addProviderTiles(providers$OpenStreetMap.Mapnik) %>%
-  
+leaflet()  %>%
+  addTiles() %>%
+  addProviderTiles(providers$Esri.WorldGrayCanvas) %>% 
   addFullscreenControl()%>%clearBounds()%>%
   addControl(title,position = "topleft",className="map-title")%>%
-  #  addControl(rr,position = "bottomleft")%>% #TABLA DE DATOS
+  
   leafem::addMouseCoordinates() %>% addFullscreenControl(position = "topleft", pseudoFullscreen = F) %>%
-  leafem::addLogo(img=imagen1$city,src="remote",position="bottomright",width=403,height=127)%>%
-  #  addDrawToolbar(editOptions = editToolbarOptions(selectedPathOptions()),polylineOptions = drawPolylineOptions(metric = T),
-  #                circleOptions = F,rectangleOptions = F,circleMarkerOptions = F) %>%
   addResetMapButton()%>%
   setView(lng = mean(camaras$LONGITUD),lat = mean(camaras$LATITUD),zoom = 13) %>%
   
@@ -146,20 +82,20 @@ leaflet()  %>%addTiles() %>%addProviderTiles(providers$Esri.WorldGrayCanvas) %>%
   #addMapPane("col",zIndex = 550)%>%
   addMapPane("lo",zIndex = 580)%>%
   
-  addPolygons(data=secXote ,color = "#7F7F7F",fillColor = "transparent",fillOpacity =0.005,weight = 3,popup = paste("<b>","SECTOR : ","</b>",secXote$SECTOR),
+  addPolygons(data=secXote ,color = "black",fillColor = "transparent",fillOpacity =0.5,weight = 3,
               labelOptions = labelOptions(noHide = T, textOnly = TRUE,direction = "center"),
               highlightOptions = highlightOptions(color = "#7F7F7F", weight = 3) , group="SECTOR",options = pathOptions(pane="polygons"))%>%
   
-  addPolygons(data=colRel,color = "#17BECF",fillColor = "#F4A460",fillOpacity =0.005,weight = 3,popup = paste("<b>","COLONIA : ","</b>",colRel$NOM_ASENTA) ,
+  addPolygons(data=colRel,color = "black",fillColor = "transparent",fillOpacity =0.5,weight = 3,
               labelOptions = labelOptions(noHide = T, textOnly = TRUE,direction = "center"),
               highlightOptions = highlightOptions(color = "#17BECF", weight = 3) , group="COLONIAS",options = pathOptions(pane="polygons"))%>%
   
   
   #STVS-------------------------------  
-  addCircles(data = cam20,lng = cam20$LONGITUD,lat = cam20$LATITUD,color = "blue" ,fillColor = "blue",radius = 8,fillOpacity = T,
-             popup = paste("<b>","ID: ","</b>",as.character(cam20$ID_BCT_O),"<br>",
-                           "<b>","TIPO DE POSTE : ","</b>",as.character(cam20$TIPO_POSTE)),
-             group = paste("20m","(",nrow(cam20),")"),options = pathOptions(pane="li"))%>%
+addCircles(data = cam20,lng = cam20$LONGITUD,lat = cam20$LATITUD,color = "blue" ,fillColor = "blue",radius = 8,fillOpacity = T,
+           popup = paste("<b>","ID: ","</b>",as.character(cam20$ID_BCT_O),"<br>",
+                         "<b>","TIPO DE POSTE : ","</b>",as.character(cam20$TIPO_POSTE)),
+           group = paste("20m","(",nrow(cam20),")"),options = pathOptions(pane="li"))%>%
   
   addCircles(data = cam9,lng = cam9$LONGITUD,lat = cam9$LATITUD,color = "red" ,fillColor ="red",radius = 10,fillOpacity = T,
              popup = paste("<b>","ID: ","</b>",as.character(cam9$ID_BCT_O),"<br>",
@@ -174,14 +110,19 @@ addCircles(data = totem, lng = totem$LONGITUD, lat = totem$LATITUD,radius = 8,co
            group = paste("TÓTEMS","(",nrow(totem),")"),options = pathOptions(pane="li"))%>%
   
   
+  #Legenda de TIPO_POSTE
+  addLegend(
+    "bottomright",
+    colors = colores_camara, # Colores correspondientes a las categorías
+    labels = etiquetas_camara, # Etiquetas de las categorías
+    title = "Cobertura") %>%
+  
+  
   #ETIQUETAS----------------------------------------------------------
 addLayersControl(overlayGroups = c( "&nbsp; <b>CAPAS</b> &nbsp; ",
-                                    "SECTORES","COLONIAS",
                                     paste("9m","(",nrow(cam9),")"),
                                     paste("20m","(",nrow(cam20),")"),
-                                    paste("TÓTEMS","(",nrow(totem),")")#,
-                                    #"&nbsp; <b>CLASIFICACI?N</b> &nbsp; ",
-                                    #paste("HOMICIDIO POR ARMA BLANCA","(",nrow(arma_blanca),")"),
+                                    paste("TÓTEMS","(",nrow(totem),")")
 ),
 options = layersControlOptions(collapsed = T))%>% 
   
@@ -209,25 +150,16 @@ options = layersControlOptions(collapsed = T))%>%
                                                  }
                                                  ")) %>%
   
-  addMarkers(data = camaras, lng = as.numeric(camaras$LONGITUD),lat = as.numeric(camaras$LATITUD), label = c(camaras$ID_BCT_O),group = 'CONS')%>%
-  addMarkers(data = totem,lng=totem$LONGITUD,lat = totem$LATITUD,label = totem$ID_BCT_O,group = "TOT")%>% 
-  addSearchFeatures(targetGroups = c("CONS","TOT"),
-                    options = searchFeaturesOptions(zoom=20, openPopup = TRUE, firstTipSubmit = TRUE,
-                                                    autoCollapse = F, hideMarkerOnCollapse = T,
-                                                    textPlaceholder="ID STV"))%>% 
-  addLegendCustom(colors, labels, sizes, shapes, borders)%>%  
-  #addLegend(position = "bottomleft",labels = c("Menos de 250 personas","250 a 599 personas","600 a 1000 personas","Más de 1000 personas"),colors = c("#FFFF00","#FFFF00","#FF0000","#FF0000"),title = "AFLUENCIA CENTRO HISTÓRICO")%>%
+  
   
   hideGroup(c( "&nbsp; <b>CAPAS</b> &nbsp; ",
-               "SECTORES","COLONIAS",
                paste("9m","(",nrow(cam9),")"),
                paste("20m","(",nrow(cam20),")"),
                paste("TÓTEMS","(",nrow(totem),")"),
-               #"&nbsp; <b>CLASIFICACI?N</b> &nbsp; ",
-               #paste("HOMICIDIO POR ARMA BLANCA","(",nrow(arma_blanca),")"),
                
                "&nbsp; <b>MI C911E</b> &nbsp; ",
                "TOT","CONS"))
+
 
 
 
